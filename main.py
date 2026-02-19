3c7af7f13612899e67db40586fac7dfe572780ec
daniya-star <daniya@eng.rizvi.edu.in>
2026-02-19 20:53:07 +0530

    Modify age output message format

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # AIM: Create a personalized greeting message
-# User enters name: shibu
 # Coder: Daniya Khan 
 # Date: 14/2/26
 
@@ -8,4 +7,4 @@ name = input("Enter your name: ")
 age = input("Enter your age: ")
 print("Hello", name + "!")
 print("Nice to meet you.")
-print("You are", age, "years old. Have a great day!")
+print("You are", age + "years old ")
