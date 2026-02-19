2fa4855dfe78bc84d375b69d85080b7725edeff1
daniya-star <daniya@eng.rizvi.edu.in>
2026-02-19 20:58:07 +0530

    Refactor greeting message and update metadata

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-# AIM: Create a personalized greeting message
-# Coder: Daniya Khan 
-# Date: 14/2/26
+#AIM: Create a personalized greeting message
 
-# Write your code here
-name = input("Enter your name: ")
-age = input("Enter your age: ")
-print("Hello", name + "!")
-print("Nice to meet you.")
-print("You are", age + "years old ")
+#Coder: Daniya Rehman Khan 
+
+#Date: 19/02/2026
+
+#Write your code here
+
+name= input("Enter Name: ")
+
+age= input("Enter Age: ")
+
+print("Welcome to Rizvi College of Engineering")
+
+print("Hello,", name)
+
+print("You are", age + " years old.")
